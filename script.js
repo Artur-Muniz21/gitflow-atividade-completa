fb541be4208dc72aca0e500ca4f10af340afee98
Artur-Muniz21 <arturvictor2107@gmail.com>
2024-05-06 13:52:18 -0300

    feat: Adição da funcionalidade multipicar no script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const calcular = document.getElementById("calcular");
 const soma = document.getElementById("somar");
 const subtracao = document.getElementById("subtracao");
 const multiplicacao = document.getElementById("multiplicacao");
-const divisao = document.getElementById("divisao");
 
 function somar(){
     const resultado = parseFloat(valorA.value) + parseFloat(valorB.value)
@@ -22,16 +21,10 @@ function multiplicar(){
     return resultado;
 };
 
-function dividir(){
-    const resultado = parseFloat(valorA.value)/parseFloat(valorB.value)
-    return resultado;
-};
-
 
 calcular.onclick = () => {
 
     soma.value = somar();
     subtracao.value = subtrair();
     multiplicacao.value = multiplicar();
-    divisao.value = dividir();
 };
