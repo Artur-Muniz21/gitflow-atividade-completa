13f65889c27c31a88d21896d9c7281d526cb4963
Artur-Muniz21 <arturvictor2107@gmail.com>
2024-05-06 13:25:59 -0300

    feat: Adição da funcionalidade subtrair no script.js

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ const valorB = document.getElementById("valorB");
 const calcular = document.getElementById("calcular")
 
 const soma = document.getElementById("somar");
+const subtracao = document.getElementById("subtracao")
 
 function somar(){
     const resultado = parseInt(valorA.value) + parseInt(valorB.value)
     return resultado;
 };
 
+function subtrair(){
+    const resultado = parseInt(valorA.value) - parseInt(valorB.value)
+    return resultado;
+};
+
 calcular.onclick = () => {
 
     soma.value = somar()
+    subtracao.value = subtrair()
 }
