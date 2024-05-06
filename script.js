3d2a52e2069ecef0f94979d06dc1cdcecb61d068
Artur-Muniz21 <arturvictor2107@gmail.com>
2024-05-06 14:09:50 -0300

    feat: Adição da funcionalidade dividir no script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const calcular = document.getElementById("calcular");
 const soma = document.getElementById("somar");
 const subtracao = document.getElementById("subtracao");
 const multiplicacao = document.getElementById("multiplicacao");
+const divisao = document.getElementById("divisao");
 
 function somar(){
     const resultado = parseFloat(valorA.value) + parseFloat(valorB.value)
@@ -21,10 +22,16 @@ function multiplicar(){
     return resultado.toFixed(2);
 };
 
+function dividir(){
+    const resultado = parseFloat(valorA.value)/parseFloat(valorB.value)
+    return resultado.toFixed(2);
+};
+
+
 calcular.onclick = () => {
 
     soma.value = somar();
     subtracao.value = subtrair();
     multiplicacao.value = multiplicar();
-
+    divisao.value = dividir();
 };
