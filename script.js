3e8795e02ccb814dbcd31e0a1b4df2b65352c046
Artur-Muniz21 <arturvictor2107@gmail.com>
2024-05-06 13:49:50 -0300

    feat: Adição da funcionalidade multipicar no script.js

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const valorA = document.getElementById("valorA");
 const valorB = document.getElementById("valorB");
-const calcular = document.getElementById("calcular")
+const calcular = document.getElementById("calcular");
 
 const soma = document.getElementById("somar");
-const subtracao = document.getElementById("subtracao")
+const subtracao = document.getElementById("subtracao");
+const multiplicacao = document.getElementById("multiplicacao");
+const divisao = document.getElementById("divisao");
 
 function somar(){
-    const resultado = parseInt(valorA.value) + parseInt(valorB.value)
+    const resultado = parseFloat(valorA.value) + parseFloat(valorB.value)
     return resultado;
 };
 
 function subtrair(){
-    const resultado = parseInt(valorA.value) - parseInt(valorB.value)
+    const resultado = parseFloat(valorA.value) - parseFloat(valorB.value)
     return resultado;
 };
 
+function multiplicar(){
+    const resultado = parseFloat(valorA.value) * parseFloat(valorB.value)
+    return resultado;
+};
+
+function dividir(){
+    const resultado = parseFloat(valorA.value)/parseFloat(valorB.value)
+    return resultado;
+};
+
+
 calcular.onclick = () => {
 
-    soma.value = somar()
-    subtracao.value = subtrair()
-}
+    soma.value = somar();
+    subtracao.value = subtrair();
+    multiplicacao.value = multiplicar();
+    divisao.value = dividir();
+};
